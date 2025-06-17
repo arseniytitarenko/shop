0f5cc4b032302faaf7b29357f6e62e0977e4bf62
titarenk0 <arseniytitarenko93@yandex.ru>
2025-06-17 22:13:55 +0300

    order-service v1

--- order-service/internal/presentation/router/router.go
+++ order-service/internal/presentation/router/router.go
@@ -7,8 +7,10 @@ import (
 
 func SetupRouter(orderHandler *handler.OrderHandler) *gin.Engine {
 	r := gin.Default()
+
 	r.POST("/orders", orderHandler.NewOrder)
 	r.GET("/orders", orderHandler.GetOrderList)
 	r.POST("/orders/status", orderHandler.GetOrderStatus)
+
 	return r
 }
